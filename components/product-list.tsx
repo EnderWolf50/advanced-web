04c22d6cdac96bcb034d495ce5c52f7eab725727
Kerati <enderwolf50@gmail.com>
2023-10-05 00:37:13 +0800

    feat: add selected state and some explainations about JS's `Array.map` method
    ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -1,17 +1,34 @@
 "use client";
 
+import { useState } from "react";
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+
 const products = [
   { desc: "iPad", price: 20000 },
   { desc: "iPhone 8", price: 20000 },
   { desc: "iPhone X", price: 30000 },
-];
+] as const;
 
 export const ProductList = () => {
+  const [selected, setSelected] = useState<number>(0);
+
   return (
     <List>
-      {products.map((product) => (
+      {/* Array.map 在 iterate 的時候，會將該次 iterate 的物品的 index 做為第二個參數傳入 */}
+      {/**
+       * Array.map(value, index, array)
+       * ? value: 當次 iterate 的值
+       * ? index: 當次 iterate 的 index
+       * ? array: 整個 array 本身
+       */}
+      {products.map((product, idx) => (
         <ListItem divider key={product.desc}>
-          <ListItemText primary={product.desc} secondary={product.price} />
+          <ListItemButton
+            selected={idx === selected}
+            onClick={() => setSelected(idx)}
+          >
+            <ListItemText primary={product.desc} secondary={product.price} />
+          </ListItemButton>
         </ListItem>
       ))}
     </List>
