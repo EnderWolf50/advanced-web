12961cf5e5855fe81d68de080375c4cd8395658f
Kerati <enderwolf50@gmail.com>
2023-10-18 15:19:55 +0800

    feat: fetch and render users and activities using RSC

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,42 @@
-import { Typography } from "@mui/material";
+import { List, ListItem, Typography } from "@mui/material";
 
-export default function Home() {
+import { getActivities, getUsers } from "@/lib/api";
+
+const SimpleList = <TItem extends { name: string }>({
+  items,
+}: {
+  items: TItem[];
+}) => {
+  return (
+    <List>
+      {items.map((item) => (
+        <ListItem key={item.name}>{item.name}</ListItem>
+      ))}
+    </List>
+  );
+};
+
+export default async function Home() {
+  const users = await getUsers();
+  const activities = await getActivities();
+
   return (
     <div>
       <Typography>This is index (/) page</Typography>
+
+      <div>
+        <Typography variant="h6" component="p">
+          Users
+        </Typography>
+        <SimpleList items={users} />
+      </div>
+
+      <div>
+        <Typography variant="h6" component="p">
+          Activities
+        </Typography>
+        <SimpleList items={activities} />
+      </div>
     </div>
   );
 }
